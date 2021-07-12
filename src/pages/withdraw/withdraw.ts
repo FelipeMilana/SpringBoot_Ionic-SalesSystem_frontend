20ceca87ea571db67b1b827706b9ff53862fe272
Felipe Milana <felipe.milana97@gmail.com>
2021-07-12 16:21:33 -0300

    Update - Withdraw page

--- src/pages/withdraw/withdraw.ts
+++ src/pages/withdraw/withdraw.ts
@@ -12,6 +12,9 @@ export class WithdrawPage {
 
   withdraws: WithdrawDTO[] = [];
   page: number = 0;
+  checkLastPage: boolean = false;
+  checkSearchBar: boolean = false;
+  search: string = '';
 
   constructor( 
     public navCtrl: NavController, 
@@ -28,13 +31,13 @@ export class WithdrawPage {
   loadData() {
     let loader = this.presentLoading();
 
-    this.withdrawService.myWithdraws(this.page)
+    this.withdrawService.myWithdraws(this.page, 10, "date", "DESC")
       .subscribe(response => {
         loader.dismiss();
-        this.withdraws.concat(response['content'] as WithdrawDTO[]);
+        this.withdraws = this.withdraws.concat(response['content'] as WithdrawDTO[]);
 
-        if(this.withdraws.length == 0) {
-          this.showFailureAlert();
+        if(response['last']) {
+          this.checkLastPage = true;
         }
       }, 
       error => {
@@ -43,9 +46,125 @@ export class WithdrawPage {
       })
   }
 
+  delete(id:string) {
+    let alert = this.alertCtrl.create({
+      title:'Aviso',
+      message:'Você deseja  deletar essa retirada?',
+      enableBackdropDismiss: false,
+      buttons:[
+        {
+          text:'Ok',
+          handler: () => {
+            let loader = this.presentLoading();
+            this.withdrawService.delete(id)
+              .subscribe(response => {
+                loader.dismiss();
+                this.showSuccessfulAlert();
+              },
+              error => {
+                loader.dismiss();
+              });
+          }
+        },
+        {
+          text:'Cancelar'
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  findByNameOrBank(string: string) {
+    let loader = this.presentLoading();
+
+    this.withdrawService.findByNameOrBank(this.page, 10, "date", "DESC", string)
+      .subscribe(response => {
+        loader.dismiss();
+        this.withdraws = this.withdraws.concat(response['content'] as WithdrawDTO[]);
+
+        if(this.withdraws.length == 0) {
+          this.showFailedSearchBar();
+        }
+
+        if(response['last']) {
+          this.checkLastPage = true;
+        }
+      },
+      error => {
+        loader.dismiss();
+      });
+  }
+
+  searchBar(event){
+    this.page = 0;
+    this.withdraws = [];
+    this.checkLastPage = false;
+    this.checkSearchBar = true;
+    this.search = event.target.value;
+    
+    this.findByNameOrBank(this.search);
+  }
+
+  cancelSearchBar() {
+    this.page = 0;
+    this.withdraws = [];
+    this.checkLastPage = false;
+    this.checkSearchBar = false;
+    this.loadData();
+  }
+
+  showSuccessfulAlert() {
+    let alert = this.alertCtrl.create({
+      title:'Sucesso',
+      message:'Retirada deletada!',
+      enableBackdropDismiss: false,
+      buttons:[
+        {
+          text:'Ok'
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  showFailedSearchBar() {
+    let alert = this.alertCtrl.create({
+      title: 'Alerta',
+      message: 'Não há retiradas',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.cancelSearchBar();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  doRefresh(refresher) {
+    this.page = 0;
+    this.withdraws = [];
+    this.checkLastPage = false;
+    this.checkSearchBar = false;
+    this.loadData();
+    
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
+  }
+
   doInfinite(infiniteScroll) {
     this.page++;
-    this.loadData();
+    
+    if(this.checkSearchBar){
+      this.findByNameOrBank(this.search);
+    }
+    else{
+      this.loadData();
+    }
     setTimeout(() => {
       infiniteScroll.complete();
     }, 1000);
@@ -57,45 +176,5 @@ export class WithdrawPage {
     });
     loader.present();
     return loader;
-  }
-
-  showFailureAlert() {
-    let alert = this.alertCtrl.create({
-      title: 'Aviso',
-      message: 'Não há retiradas registradas!',
-      enableBackdropDismiss: false,
-      buttons: [
-        {
-          text:'Ok',
-          handler: () => {
-           this.askWantAdd();
-          }
-        }
-      ]
-    });
-    alert.present();
-  }
-
-  askWantAdd() {
-    let alert = this.alertCtrl.create({
-      title: 'Aviso',
-      message: 'Deseja fazer uma retirada?',
-      enableBackdropDismiss: false,
-      buttons: [
-        {
-          text:'Ok',
-          handler: () => {
-            this.navCtrl.push('');
-          }
-        },
-        {
-          text:'Cancel',
-          handler: () => {
-            this.navCtrl.pop();
-          }
-        }
-      ]
-    });
-    alert.present();
   }
 }
