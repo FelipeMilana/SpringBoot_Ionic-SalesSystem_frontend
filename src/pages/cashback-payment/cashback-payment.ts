8d02cf7fb8afc4b9ad02f7fbc03a7b1bcb291595
Felipe Milana <felipe.milana97@gmail.com>
2021-08-05 14:16:26 -0300

    CashbackPayment page

--- src/pages/cashback-payment/cashback-payment.ts
+++ src/pages/cashback-payment/cashback-payment.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the CashbackPaymentPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController, IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
+import { ExchangeVehicleDTO } from '../../models/exchangeVehicleDTO';
+import { PersonDTO } from '../../models/personDTO';
+import { SaleService } from '../../services/domain/sale.service';
 
 @IonicPage()
 @Component({
@@ -14,12 +11,92 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'cashback-payment.html',
 })
 export class CashbackPaymentPage {
+  
+  person: PersonDTO;
+  vehicleId: string;
+  finalValue: number;
+  exchangeVehicle: ExchangeVehicleDTO;
+  formGroup: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
+    public saleService: SaleService) {
+
+      this.person = this.navParams.get('person');
+      this.vehicleId = this.navParams.get('vehicleId');
+      this.finalValue = this.navParams.get('finalValue');
+      this.exchangeVehicle = this.navParams.get('exchangeVehicle');
+
+      this.formGroup = this.formBuilder.group({
+        id: [null, []],
+        finalValue: [this.finalValue, []],
+        payment: this.formBuilder.group({
+          cashback: [this.exchangeVehicle.paidValue - this.finalValue, []],
+          bank: ['', [Validators.required]],
+          exchangeVehicle: [this.exchangeVehicle, []],
+          "@type": ['pagamentoTrocaComTroco', []]
+        }),
+        client: [this.person, []]
+      });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CashbackPaymentPage');
+  insert() {
+    let alert = this.alertCtrl.create({
+      title: 'Aviso',
+      message: 'Deseja realizar essa venda?',
+      enableBackdropDismiss: false,
+      buttons:[
+        {
+          text:'Ok',
+          handler: () => {
+            let loader = this.presentLoading();
+
+            this.saleService.insert(this.formGroup.value, this.vehicleId)
+              .subscribe(response => {
+                loader.dismiss();
+                this.showSuccessFulAlert();
+              },
+              error => {
+                loader.dismiss();
+              });
+          }
+        },
+        {
+          text:'Cancel'
+        }
+      ]
+    });
+    alert.present();
+    
+  }
+
+  showSuccessFulAlert() {
+    let alert = this.alertCtrl.create({
+      title:'Sucesso',
+      message:'Venda Realizada!',
+      enableBackdropDismiss: false,
+      buttons:[
+        {
+          text:'Ok',
+          handler: () => {
+            this.navCtrl.setRoot('MainPage');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content:'Aguarde...',
+    });
+    loader.present();
+    return loader;
   }
 
 }
--- src/services/domain/sale.service.ts
+++ src/services/domain/sale.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { API_CONFIG } from "../../configs/api.config";
+import { SaleDTO } from "../../models/saleDTO";
 
 @Injectable()
 export class SaleService {
@@ -11,5 +12,14 @@ export class SaleService {
 
     mySales(page: number, size: number, sort: string, direction: string) {
         return this.http.get(`${API_CONFIG.baseURL}/sales?page=${page}&&size=${size}&&sort=${sort}&&direction=${direction}`);
+    }
+
+    insert(obj: SaleDTO, id: string) {
+        return this.http.post(`${API_CONFIG.baseURL}/sales/${id}`,
+        obj,
+        {
+            observe:'response',
+            responseType:'text'
+        });
     }
 }
