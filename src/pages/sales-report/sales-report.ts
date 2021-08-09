ec6efe85a98163a1309f3e4abc283fe52bc54d5b
Felipe Milana <felipe.milana97@gmail.com>
2021-08-09 16:06:57 -0300

    SalesReport page

--- src/pages/sales-report/sales-report.ts
+++ src/pages/sales-report/sales-report.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController, IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
+import { SaleDTO } from '../../models/saleDTO';
+import { SaleService } from '../../services/domain/sale.service';
 
 @IonicPage()
 @Component({
@@ -10,18 +12,86 @@ export class SalesReportPage {
 
   startDate: string;
   endDate: string;
+  sales: SaleDTO[] = [];
+  page: number = 0;
+  checkLastPage: boolean = false;
+  totalProfit: number;
+  ticketValue: number;
 
   constructor(
     public navCtrl: NavController, 
-    public navParams: NavParams) {
+    public navParams: NavParams, 
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public saleService: SaleService) {
 
       this.startDate = this.navParams.get('startDate');
       this.endDate = this.navParams.get('endDate');
   }
 
   ionViewDidLoad() {
-    console.log(this.startDate);
-    console.log( this.endDate);
+    this.loadData();
   }
 
+  loadData() {
+    let loader = this.presentLoading();
+
+    this.saleService.monthlyReport(this.page, 10, "date", "ASC", this.startDate, this.endDate)
+      .subscribe(response => {
+        loader.dismiss();
+        this.sales = this.sales.concat(response['content'] as SaleDTO);
+
+        if(response['last']) {
+          this.checkLastPage = true;
+        }
+
+        this.totalProfit = 0;
+        this.ticketValue = 0;
+        for(var i=0; i<this.sales.length; i++) {
+          this.totalProfit = this.totalProfit + this.sales[i].profit;
+        }
+
+        this.ticketValue = this.totalProfit / this.sales.length;
+        
+        this.showEmailOk();
+      },
+      error => {
+        loader.dismiss();
+      });
+  }
+
+  showDetails(id: string) {
+    this.navCtrl.push('SaleDetailsPage', {saleId: id});
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    
+    setTimeout(() => {
+      infiniteScroll.complete();
+    }, 1000);
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: 'Aguarde...',
+    });
+    loader.present();
+    return loader;
+  }
+
+  showEmailOk() {
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso',
+      message: 'Relatório enviado para o seu email!',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+        }
+      ]
+    });
+    alert.present();
+  }
 }
--- src/services/domain/sale.service.ts
+++ src/services/domain/sale.service.ts
@@ -15,11 +15,15 @@ export class SaleService {
     }
 
     findByVehicleModelOrLicensePlate(page: number, size: number, sort: string, direction: string, str: string) {
-        return this.http.get(`${API_CONFIG.baseURL}/sales/vehicleModelOrLicensePlate?value=${str}&&page=${page}&&size=${size}&&sort=${sort}&&direction=${direction}`)
+        return this.http.get(`${API_CONFIG.baseURL}/sales/vehicleModelOrLicensePlate?value=${str}&&page=${page}&&size=${size}&&sort=${sort}&&direction=${direction}`);
     }
 
     findById(id: string) {
         return this.http.get(`${API_CONFIG.baseURL}/sales/${id}`);
+    }
+
+    monthlyReport(page: number, size: number, sort: string, direction: string, startDate: string, endDate: string) {
+        return this.http.get(`${API_CONFIG.baseURL}/sales/monthlyReport?startDate=${startDate}&&endDate=${endDate}&&page=${page}&&size=${size}&&sort=${sort}&&direction=${direction}`);
     }
 
     insert(obj: SaleDTO, id: string) {
